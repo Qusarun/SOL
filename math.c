8a2078a18201efe56fc37f22758b12e80022df7d
Qusarun <94105803+Qusarun@users.noreply.github.com>
2022-01-25 20:39:04 +0300

    Create math.c

---
+++ math.c
@@ -0,0 +1,5 @@
+#include <stdio.h>       // #include <stdio.h>
+int main(void) {         // int main(void) {
+    printf("%d\n", 1+2); //     printf("%d\n", 3);
+    return 0;            //     return 0;
+}                        // }
